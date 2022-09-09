58cbacd62aebe71feb6c00260b7f5ca8373631ac
nicolaser55 <nicolaser55@gmail.com>
2022-09-09 23:00:52 +0300

    added recursive combinations code

--- combinatronics.py
+++ combinatronics.py
@@ -1,4 +1,5 @@
-prices = [5, 18,25,25, 50, 23,12, 12, 12,
+prices = [5, 18,25,25,
+                # 50, 23,12, 12, 12,
                 # 10,3,25,30,30,25,12,25,18,
                 # 18,18,18,18,25,15,22,10,30,
                 # 40,25,25,28,28,28,28,50,25,
@@ -16,13 +17,29 @@ def set_check(list):
         # RETURN FALSE BECAUSE IT DOES NOT FIT
         return False
 
-# ITERATE OVER PRICES
-def combinatronics(prices):
-    # CREATE LIST OF CORRECT SETS
-    correct_sets = []
-    # ITERATE OVER NUMBERS
-    for number in prices:
-        print(number)
+def n_length_combo(lst, n):
+    # IF N IS EQUAL TO 0
+    if n == 0:
+        # RETURN LIST OF LIST
+        return [[]]
+    # LIST OF COMBINATIONS
+    l =[]
+    # ITERATE OVER LIST RANGE
+    for i in range(0, len(lst)):
+        # M IS THE ITEM AT INDEX
+        m = lst[i]
+        # REMAINING LIST
+        remLst = lst[i + 1:]
+        # SET VARIABLE RECURSIVELY
+        remainlst_combo = n_length_combo(remLst, n-1)
+        # ITERATE OVER REMAINING NUMBERS
+        for p in remainlst_combo:
+            # APPEND M, *P TO LIST VARIABLE
+            l.append([m, *p])
+            print([m, *p])
 
+    return l
 
-combinatronics(prices)
+print(n_length_combo(prices, 4))
+
+# print(correct_sets)
